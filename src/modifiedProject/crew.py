219e9f6a43ed3317ee6afc70063cc0d2d0caeb56
Ezdehar-Jayosi <ezdeharj.95@gmail.com>
2025-10-19 16:25:02 +0300

    first commit

---
+++ src/modifiedProject/crew.py
@@ -0,0 +1,103 @@
+import os
+import json
+from crewai import LLM
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+
+
+from pydantic import BaseModel
+from jambo import SchemaConverter
+
+@CrewBase
+class modifiedProject:
+    """UntitledProject crew"""
+
+    
+    @agent
+    def information_collector(self) -> Agent:
+
+        
+        return Agent(
+            config=self.agents_config["information_collector"],
+            
+            
+            tools=[
+
+            ],
+            reasoning=False,
+            max_reasoning_attempts=None,
+            inject_date=True,
+            allow_delegation=False,
+            max_iter=25,
+            max_rpm=None,
+            max_execution_time=None,
+            llm=LLM(
+                model="gpt-4o-mini",
+                temperature=0.7,
+            ),
+            
+        )
+    
+    @agent
+    def information_summarizer(self) -> Agent:
+
+        
+        return Agent(
+            config=self.agents_config["information_summarizer"],
+            
+            
+            tools=[
+
+            ],
+            reasoning=False,
+            max_reasoning_attempts=None,
+            inject_date=True,
+            allow_delegation=False,
+            max_iter=25,
+            max_rpm=None,
+            max_execution_time=None,
+            llm=LLM(
+                model="gpt-4o-mini",
+                temperature=0.7,
+            ),
+            
+        )
+
+
+    
+    @task
+    def collect_personal_project_information(self) -> Task:
+        return Task(
+            config=self.tasks_config["collect_personal_project_information"],
+            markdown=False,
+            output_json=self._load_response_format("collect_personal_project_information"),
+            human_input=True
+            
+        )
+    
+    @task
+    def create_person_summary(self) -> Task:
+        return Task(
+            config=self.tasks_config["create_person_summary"],
+            markdown=False,
+            
+            
+        )
+    
+    
+
+    @crew
+    def crew(self) -> Crew:
+        """Creates the UntitledProject crew"""
+        return Crew(
+            agents=self.agents,  # Automatically created by the @agent decorator
+            tasks=self.tasks,  # Automatically created by the @task decorator
+            process=Process.sequential,
+            verbose=True,
+        )
+
+    def _load_response_format(self, name):
+        with open(os.path.join(self.base_directory, "config", f"{name}.json")) as f:
+            json_schema = json.loads(f.read())
+
+        return SchemaConverter.build(json_schema)
